126b2b75ae29145857cb178e05ba8630bcc61631
Nehalspeaks <Nehalspeaks@gmail.com>
2024-06-18 23:36:26 +0530

    Add Three Nums

---
+++ ADDThree.py
@@ -0,0 +1,13 @@
+#Adding Three Numbers
+
+print("Enter First Number :")
+a=float(input())
+print("Enter Second Number :")
+b=float(input())
+print("Enter Third Value :")
+c=float(input())
+print("First Number :{}".format(a))
+print("Second Number :{}".format(b))
+print("Third Number :{}".format(c))
+
+print("Addition of ({},{} and {}) ={}".format(a,b,c,a+b+c))
