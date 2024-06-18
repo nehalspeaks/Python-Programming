d588c8882197eab61ee353d6fc670c0591375e7f
Nehalspeaks <Nehalspeaks@gmail.com>
2024-06-18 23:32:29 +0530

    Add Two Nums

---
+++ Add.py
@@ -0,0 +1,10 @@
+#Adding Two Numbers
+
+print("Enter First Number :")
+a=float(input())
+print("Enter Second Number :")
+b=float(input())
+
+print("First Number :{}".format(a))
+print("Second Number :{}".format(b))
+print("Addition of ({} and {}) ={}".format(a,b,a+b))
