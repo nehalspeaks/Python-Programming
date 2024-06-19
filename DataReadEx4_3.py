314d51a3832e2499d48f568a54772e3dacc964fa
nehalspeaks <100505365+nehalspeaks@users.noreply.github.com>
2024-06-19 12:55:03 +0530

    Add files via upload

---
+++ DataReadEx4_3.py
@@ -0,0 +1,14 @@
+# Program for multiplying TWO Numbers
+
+print("-"*50)
+a=input("Enter First Value : ")
+b=input(("Enter Second Value : "))
+
+#Reading the data from the keyboard and converting
+x=float(a)
+y=float(b)
+z=x*y
+
+print("-"*50)
+print("Multiplication of ({} and {})={}".format(x,y,z))
+print("-"*50)
---
+++ DataReadEx4_4.py
@@ -0,0 +1,3 @@
+# Program for Multiplying TWO Numbers In Less Lines of Code
+
+print("Multiplication =  {}".format(float(input("Enter First Value :"))*float(input("Enter Second Value :"))))
