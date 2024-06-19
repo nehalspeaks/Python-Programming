7518d40927ab8c1139f3dfb1efb82ebad56015a7
Nehalspeaks <Nehalspeaks@gmail.com>
2024-06-19 12:11:20 +0530

    Program for multiplying TWO Numbers

--- DataReadEx4_2.py
+++ DataReadEx4_2.py
@@ -1,16 +1,16 @@
 # Program for Multiplying Two Numbers using Third variable
 
 print("Enter First Value :")
-a=float(input())
+a = float(input())
 print("Enter Second Value :")
-b=float(input())
+b = float(input())
 
-c=a*b
+c = a * b
 
-print("-"*50)
+print("-" * 50)
 print("First Value :{} ".format(a))
 print("Second Value  :{} ".format(b))
-print("-"*50)
-print("Multiplication of {} and {} = {}".format(a,b,c))
+print("-" * 50)
+print("Multiplication of {} and {} = {}".format(a, b, c))
 
-print("-"*50)
+print("-" * 50)
--- DownloadPlayList.py
+++
@@ -1,7 +0,0 @@
-from pytube import Playlist
-
-playlist_url=input("Enter Playlist URL :")
-playlist=Playlist(playlist_url)
-
-for video in playlist.videos:
-    video.streams.get_highest_resolution().download()
