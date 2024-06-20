b76b16f9c941495dc4fbe35c9a81f3aec21296e5
Nehalspeaks <Nehalspeaks@gmail.com>
2024-06-20 23:17:13 +0530

    Logical Operators

--- DataReadEx1.py
+++
@@ -1,6 +0,0 @@
-#First Program
-#Program for Data Reading
-
-print("Enter Any Value :")  # Helps for User-Prompting
-x=input()
-print(x,type(x))
--- DataReadEx2.py
+++
@@ -1,4 +0,0 @@
-#Program for getting the data from keyboard
-
-x=input("Enter Any Value :")
-print(x,type(x))
